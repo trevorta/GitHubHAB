bb16efa903caf3fc8a33c505fa2918b4cf09c01b
trevorta <tahientriet@gmail.com>
2014-09-10 21:11:06 -0700

    Trolled

--- Work.java
+++ Work.java
@@ -1,5 +1,12 @@
 public class Work {
     public static void main(String... args) {
 	System.out.println("Changed it!");
+	infiniteLoop();
+    }
+
+    public static void infiniteLoop() {
+	while (true) {
+	    System.out.println("Trolled!!!!!");
+	}
     }
 }
